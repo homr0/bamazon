54d0daf5037d1316ab402e75ddc211415b65213a
Rebecca Hom <rebecca.hom001@gmail.com>
2018-12-19 17:00:05 -0800

    added in Bamazon customer view

---
+++ bamazonCustomer.js
@@ -0,0 +1,118 @@
+// Requirements
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+// Global variables
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+// Connects to database. 
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+    bamazonInit();
+});
+
+// Initial screen for Bamazon.
+var bamazonInit = () => {
+    console.log("Welcome to Bamazon!");
+    bamazonAll();
+}
+
+// Shows all products from Bamazon.
+var bamazonAll = () => {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        console.log("----- ALL BAMAZON PRODUCTS -----\n");
+
+        for(var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+        }
+
+        bamazonOrder();
+    });
+}
+
+// Allows user to order a Bamazon product.
+var bamazonOrder = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Product ID:",
+            name: "item"
+        },
+
+        {
+            type: "input",
+            message: "Quantity:",
+            name: "quantity"
+        }
+    ]).then(function(inquiry) {
+        // Gets the product information.
+        connection.query("SELECT product_name, price, stock_quantity FROM products WHERE ?", {
+            item_id: inquiry.item
+        }, function(err, res) {
+            if (err) throw err;
+
+            if(res.length > 0) {
+                // If the quantity ordered is less than what is in stock, then update the quantity.
+                var item = res[0];
+                var remaining = item.stock_quantity - inquiry.quantity;
+                if(remaining >= 0) {
+                    connection.query("UPDATE products SET ? WHERE ?", [
+                        {
+                            stock_quantity: remaining
+                        },
+
+                        {
+                            item_id: inquiry.item
+                        }
+                    ], function(err, res) {
+                        if(err) throw err;
+
+                        // Tells the user how much their purchase was.
+                        console.log("You bought "
+                        + inquiry.quantity + " "
+                        + item.product_name
+                        + ((inquiry.quantity > 1) ? "s " : " ") 
+                        + "for $"
+                        + (parseFloat(inquiry.quantity * item.price).toFixed(2))
+                        + ".");
+                        bamazonEnd();
+                    });
+                } else {
+                    console.log("Insufficient quantity!\nPlease try ordering again with a smaller quantity.");
+                    bamazonOrder();
+                }
+            } else {
+                console.log("That is not a valid product ID.");
+                bamazonOrder();
+            }
+        });
+    });
+}
+
+// Ends the Bamazon application and closes the connection.
+var bamazonEnd = () => {
+    // Asks if the user wants to order another item or if they are done ordering.
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to place another order?",
+            name: "order"
+        }
+    ]).then(function(restart) {
+        if(restart.order) {
+            bamazonOrder();
+        } else {
+            console.log("Thank you for shopping at Bamazon!");
+            connection.end();
+        }
+    });
+}
