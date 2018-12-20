4c42b5f614da8afb8295300fbc8426648dbd25cf
Rebecca Hom <rebecca.hom001@gmail.com>
2018-12-19 17:02:30 -0800

    updated the product names

--- bamazon.sql
+++ bamazon.sql
@@ -17,12 +17,12 @@ CREATE TABLE products(
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
     VALUES ("Rilakkuma plush", "toys and games", 24.99, 10),
-    ("Aquarium tank (medium)", "pet supplies", 79.99, 12),
-    ("Sherpa throw (maroon)", "home and kitchen", 16.49, 20),
+    ("Medium aquarium tank", "pet supplies", 79.99, 12),
+    ("Maroon sherpa throw", "home and kitchen", 16.49, 20),
     ("Raccoon chew toy", "pet supplies", 14.98, 6),
-    ("Printer paper (500 sheets)", "office supplies", 8.99, 9),
-    ("Sharpie pens (variety pack)", "office supplies", 10.99, 21),
+    ("Printer paper 500-sheets pack", "office supplies", 8.99, 9),
+    ("Sharpie pens variety pack", "office supplies", 10.99, 21),
     ("Micro USB cable", "electronics", 4.99, 97),
     ("Gloomhaven", "toys and games", 139.99, 4),
-    ("Throw pillows", "home and kitchen", 12.99, 34),
+    ("Throw pillow", "home and kitchen", 12.99, 34),
     ("Power strip", "electronics", 6.99, 8);
